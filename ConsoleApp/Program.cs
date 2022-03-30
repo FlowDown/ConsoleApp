5e92e70a4bab2e37e51490f495113b3529c3f66e
UgEvgenii <adisey.07@yandex.ru>
2022-03-30 20:45:47 +0300

    Добавлена функция с получением строки из массива строк с длинной <=3 символам

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,3 +2,19 @@
 string[] arr1 = {"hello","2","world","world",":-)" };
 string[] arr2 = { "1234", "1567", "-2", "computer science" };
 string[] arr3 = { "Russia", "Denmark", "Kazan" };
+
+Console.WriteLine(GetString(arr1));
+
+static string GetString(string [] arr)
+{
+    string strArr="";
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            strArr = strArr + arr[i] + ",";
+        }
+        if (strArr != "") strArr = strArr.Remove(strArr.Length-1);
+    }
+    return strArr;
+}
