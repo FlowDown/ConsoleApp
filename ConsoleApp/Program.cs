572914b1c26f9346fee6d1af0cb919785ea83e59
UgEvgenii <adisey.07@yandex.ru>
2022-03-30 20:35:01 +0300

    Добавлены массивы данных

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] arr1 = {"hello","2","world","world",":-)" };
+string[] arr2 = { "1234", "1567", "-2", "computer science" };
+string[] arr3 = { "Russia", "Denmark", "Kazan" };
